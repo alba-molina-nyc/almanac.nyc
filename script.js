765e3347a79128454c244190ecb21d7ec25627c9
alba-molina-nyc <avmanyc@gmail.com>
2021-12-11 17:49:05 -0500

    steps

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 // Variables and Constants
 
 // Cached Element References
-const BASE_URL = 'https://data.cityofnewyork.us/resource/'; 
+const BASE_URL = 'https://data.cityofnewyork.us/'; 
 
-const API_KEY = ''
+// const API_KEY = ''
 
 
 // Event Listeners
 
 // Functions
+//function dedicated to getting data from API 
+function getData() {
+    //use $.ajax to fetch data from the api
+    //save data locally 
+    //transfer the data to the DOM 
+
+}
