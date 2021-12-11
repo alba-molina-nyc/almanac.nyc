7840ff0509f6003d059a5d1833b610510a841e5b
alba-molina-nyc <avmanyc@gmail.com>
2021-12-11 18:38:42 -0500

    wip adds function get data able to get data

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Variables and Constants
+const BASE_URL = 'https://data.cityofnewyork.us/resource/pqg4-dm6b.json';
 
 // Cached Element References
 
@@ -8,16 +9,28 @@
 
 // Functions
 
+function getData() {
+    $.ajax(`${BASE_URL}`)
+    .then(function(data){
+        alert("Retrieved " + data.length + " records from the dataset!");
+        console.log(data);
+    }, function(error) {
+        alert("Error could not retrieve data");
+        console.log(error);
+    });
+
+}
 
 
-$.ajax({
-    url: "https://data.cityofnewyork.us/resource/pqg4-dm6b.json",
-    type: "GET",
-    data: {
-      "$limit" : 5000,
-      "$$app_token" : "WTGqxDQfQdZayWp451rtfa2w9"
-    }
-}).done(function(data) {
-  alert("Retrieved " + data.length + " records from the dataset!");
-  console.log(data);
-});
+
+// $.ajax({
+//     url: "https://data.cityofnewyork.us/resource/pqg4-dm6b.json",
+//     type: "GET",
+//     data: {
+//       "$limit" : 5000,
+//       "$$app_token" : "WTGqxDQfQdZayWp451rtfa2w9"
+//     }
+// }).done(function(data) {
+//   alert("Retrieved " + data.length + " records from the dataset!");
+//   console.log(data);
+// });
