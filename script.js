ee02d8bd21149cce8d1c55cb1cc1865885ab86d2
alba-molina-nyc <avmanyc@gmail.com>
2021-12-11 19:13:03 -0500

    getresource data works

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Variables and Constants
+
+let resourceData;
+
 const BASE_URL = 'https://data.cityofnewyork.us/resource/pqg4-dm6b.json';
 
 // Cached Element References
@@ -9,15 +12,29 @@ const BASE_URL = 'https://data.cityofnewyork.us/resource/pqg4-dm6b.json';
 
 // Functions
 
-function getData() {
+function getResourceData() {
     $.ajax(`${BASE_URL}`)
-    .then(function(data){
-        alert("Retrieved " + data.length + " records from the dataset!");
-        console.log(data);
+    .then(function(data) {
+        resourceData = data;
+        render();
     }, function(error) {
-        alert("Error could not retrieve data");
         console.log(error);
     });
-
 }
+
+function render() {
+    console.log(resourceData)
+}
+
+// function getData() {
+//     $.ajax(`${BASE_URL}`)
+//     .then(function(data){
+//         resourceData = data;
+//         alert("Retrieved " + data.length + " records from the dataset!");
+//         console.log(data);
+//     }, function(error) {
+//         alert("Error could not retrieve data");
+//         console.log(error);
+//     });
+// }
 
