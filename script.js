223cbb2dc4e98ae87d136619329d4cb4b8f47815
alba-molina-nyc <avmanyc@gmail.com>
2021-12-12 18:24:27 -0500

    it connects to the API but unable to render still

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 
 let resourceData;
 
+let allData;
+
+let allOrganizationNames = [];
+
 const BASE_URL = 'https://data.cityofnewyork.us/resource/pqg4-dm6b.json';
 
-// Cached Element References
+const $search_value = $('#myInput')
 
+
+// Cached Element References
 
 // Event Listeners
 
@@ -35,4 +41,27 @@ const BASE_URL = 'https://data.cityofnewyork.us/resource/pqg4-dm6b.json';
     
             $('main').html(html);
         }
+
+
+
+        // -------
+        // -----
+
+   
+
+
+const $myInput = $('myInput');        
+
+$myInput.on('keyup', searchData);
+        
+function searchData() {
+    let search_value = $myInput;
+    $.ajax(`${BASE_URL}?organizationname=${search_value}`).then(function(data) {
+        resourceData = data;
+        console.log(data)
+        render();
+    }, function(error) {
+        console.log(error);
+    });
+}
 
