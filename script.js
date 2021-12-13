b8d11b2a6fc1e9c4b68422fff811cf04dd7f9a80
alba-molina-nyc <avmanyc@gmail.com>
2021-12-13 16:03:03 -0500

    done

--- script.js
+++ script.js
@@ -1,53 +1,65 @@
 // Variables and Constants
 
 let resourceData;
+let userInput;
+
 
 const BASE_URL = 'https://data.cityofnewyork.us/resource/pqg4-dm6b.json';
 
-const $organizationname = $('#organizationname');
 
-const $url = $('#url');
-
-const $input = $('input[type="text"]');
 // Cached Element References
+
+
+const $resourcename = $('#name');
+const $url = $('#url');
+const $address1 = $('#address1');
+const $input = $('input[type="text"]');
 
 // Event Listeners
 
 // Functions
-    function showResources() {
-        $.ajax(`${BASE_URL}`)
-        .then(function(data) {
-            resourceData = data;
-            render();
-        }, function(error) {
-            console.log(error);
+function showResources() {
+    $.ajax(`${BASE_URL}`)
+    .then(function(data) {
+        resourceData = data;
+        render();
+    }, function(error) {
+        console.log(error);
+    });
+}
+    function render() {
+        const html = resourceData.map(function(resource) {
+            return `<div class="row">
+            <div class="col s12 m6" id="article">
+              <div class="card-panel teal" id="article1">
+                <span class="white-text"> <h4>${resource.organizationname}</h4>
+                    <a target="_blank" href="${resource.url}">${resource.organizationname} Website </a>
+                    <p>${resource.address1}</p>
+                </span>
+              </div>
+            </div>
+          </div>`;
         });
+
+        $('main').html(html);
+
     }
-        function render() {
-            const html = resourceData.map(function(resource) {
-                return `<div class="row">
-                <div class="col s12 m6" id="article">
-                  <div class="card-panel teal" id="article1">
-                    <span class="white-text"> <h4>${resource.organizationname}</h4>
-                        <a target="_blank" href="${resource.url}">${resource.organizationname} Website </a>
-                        <p>${resource.address1}</p>
-                    </span>
-                  </div>
-                </div>
-              </div>`;
-            });
-    
-            $('main').html(html);
-        }
 
+    $('form').on('submit', handleGetData);
+    function handleGetData(event) {
+        event.preventDefault();
+           // calling preventDefault() on a 'submit' event will prevent a page refresh  
+        userInput = $input.val();
+$.ajax(`${BASE_URL}?description=` + userInput).then(
+      (data) => {
+          resourceData = data;
+          render();
+          
+       console.log(data);
+      },
+      (error) => {
+       console.log('bad request: ', error);
+      }
+    );
+    }
 
-
-        // -------
-        // -----
-
-        let description_value= '';
-
-        const promise = $.ajax(`${BASE_URL}?description=Traumatic Brain Injury/Coma Recovery`).done(function(data) {
-            alert("Retrieved " + data.length + " records from the dataset!");
-            console.log(data);
-          });
